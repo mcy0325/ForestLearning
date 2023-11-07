656911427368f6baf7c1ac5e307f86a51378cc61
kimbodle <103980156+kimbodle@users.noreply.github.com>
2023-11-08 02:56:16 +0900

    23.11.08 bo bottom_navi add

--- app/src/main/java/com/example/forestlearning/MainActivity.kt
+++ app/src/main/java/com/example/forestlearning/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.forestlearning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.forestlearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val nevController = binding.frgNav3.getFragment<NavHostFragment>().navController
+        //setupActionBarWithNavController(navController)
+        binding.bottomNavi.setupWithNavController(nevController)
+        setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = binding.frgNav3.getFragment<NavHostFragment>().navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }

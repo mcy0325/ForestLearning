5597af7cf183e747b72679318c04e67b731b0681
KBK <rh2168@naver.com>
2023-11-13 01:26:21 +0900

    23.11.12 bo menu ro tlqkf

--- app/src/main/java/com/example/forestlearning/MainActivity.kt
+++ app/src/main/java/com/example/forestlearning/MainActivity.kt
@@ -3,24 +3,34 @@ package com.example.forestlearning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.forestlearning.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     lateinit var binding : ActivityMainBinding
+    lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val nevController = binding.frgNav3.getFragment<NavHostFragment>().navController
-        //setupActionBarWithNavController(navController)
-        binding.bottomNavi.setupWithNavController(nevController)
+        val nevController = binding.frgNavi.getFragment<NavHostFragment>().navController
+        appBarConfiguration = AppBarConfiguration(
+           // nevController.graph, binding.drawerLayout
+            setOf(R.id.timetableFragment, R.id.fruitshowFragment,R.id.calendarFragment,
+                R.id.todaytreeFragment,R.id.studytimeFragment,R.id.todolistFragment),
+            binding.drawerLayout
+        )
+        setupActionBarWithNavController(nevController,appBarConfiguration)
+        //binding.bottomNavi.setupWithNavController(nevController)
+        binding.drawerNav.setupWithNavController(nevController)
         setContentView(binding.root)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = binding.frgNav3.getFragment<NavHostFragment>().navController
-        return navController.navigateUp() || super.onSupportNavigateUp()
+    override fun onSupportNavigateUp(): Boolean{
+        val navController = binding.frgNavi.getFragment<NavHostFragment>().navController
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }

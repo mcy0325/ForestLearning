fce6dd4cf7d271012b2ffc8122754a576dfd123b
kimbodle <103980156+kimbodle@users.noreply.github.com>
2023-11-14 23:48:19 +0900

    23.11.14 bo todolist FirebaseDatabase upload per email add, navi Calendar delete

--- app/src/main/java/com/example/forestlearning/TodoadderFragment.kt
+++ app/src/main/java/com/example/forestlearning/TodoadderFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.forestlearning.databinding.FragmentTodoadderBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -25,27 +26,29 @@ class TodoadderFragment : Fragment() { //사용자가 데이터 추가하는 프
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        databaseReference = FirebaseDatabase.getInstance().getReference("todos")
+        // 현재 로그인된 사용자의 UID 가져오기
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val uid = currentUser?.uid
 
-        val saveButton = binding.saveButton
-        saveButton.setOnClickListener {
-            val title = binding.titleEditText.text.toString()
+        // UID가 null이 아닌 경우에만 데이터베이스 경로 생성
+        uid?.let { userUid ->
+            // 사용자 별로 투두를 저장할 경로 설정
+            databaseReference = FirebaseDatabase.getInstance().getReference("todos").child(userUid)
 
-            // ToDo아이템을 TodoAddFragment에서 받아오기
-            val newTodoItem = Todo(content = title)
-            // Firebase Realtime Database에 데이터 업로드
-            val newTodoRef = databaseReference.push()
-            newTodoRef.setValue(newTodoItem)
+            val saveButton = binding.saveButton
+            saveButton.setOnClickListener {
+                val title = binding.titleEditText.text.toString()
 
-            /*
-            // 부모 Fragment에 데이터 전달
-            val parentFragment = parentFragment
-            if (parentFragment is TodolistFragment) {
-                parentFragment.onTodoItemAdded(newTodoItem)
+                // ToDo아이템을 TodoAddFragment에서 받아오기
+                val newTodoItem = Todo(content = title)
+
+                // Firebase Realtime Database에 데이터 업로드
+                val newTodoRef = databaseReference.push()
+                newTodoRef.setValue(newTodoItem)
+
+                // TodoAddFragment를 스택에서 제거하여 이전 화면으로 돌아가기
+                parentFragmentManager.popBackStack()
             }
-             */
-            // TodoAddFragment를 스택에서 제거하여 이전 화면으로 돌아가기
-            parentFragmentManager.popBackStack()
         }
     }
 

f094673789576e8a73ccfc1dbc0a1801982de8bf
Mun Chae Yeong <99884555+mcy0325@users.noreply.github.com>
2023-11-15 23:25:17 +0900

    11.15. mcy3 로그인 수정

--- app/src/main/java/com/example/forestlearning/repository/TimetableRepository.kt
+++ app/src/main/java/com/example/forestlearning/repository/TimetableRepository.kt
@@ -7,30 +7,25 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 
 class TimetableRepository {
     val database = Firebase.database
-    val userRef = database.getReference("user")
+    val db : FirebaseFirestore = Firebase.firestore
 
-    fun observeCourseData(courseData: MutableLiveData<ArrayList<CourseData>>) {
-        userRef.addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val users = ArrayList<CourseData>()
-                for(snap in snapshot.children){
-                    val user = snap.getValue(CourseData::class.java)
-                    user?.let {
-                        users.add(it)
-                    }
-                }
+    //realtime에서 사용자 데이터를 가져올 변수들
 
-                courseData.postValue(users)
+    val courseNameRef = database.getReference("CourseName")
+    val teacherNameRef = database.getReference("TeacherName")
+    val day1Ref = database.getReference("Day1")
+    val time1Ref = database.getReference("Time1")
+    val time2Ref = database.getReference("Time2")
+    val coursePlace1Ref = database.getReference("CoursePlace1")
+    val day2Ref = database.getReference("Day2")
+    val time3Ref = database.getReference("Time3")
+    val time4Ref = database.getReference("Time4")
+    val coursePlace2Ref = database.getReference("CoursePlace2")
 
-            }
-            override fun onCancelled(error: DatabaseError) {
-            }
-        })
-    }
-    fun addCourseData(data : CourseData){
-        userRef.push().setValue(data)
-    }
+
 }

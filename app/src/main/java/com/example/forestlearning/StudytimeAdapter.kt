4361f47d6c62b1d24809a37eab861c998d7659df
lif_eposi <104194878+lifeposi@users.noreply.github.com>
2023-11-21 09:39:43 +0900

    #5 studytime update timer update완료

--- app/src/main/java/com/example/forestlearning/StudytimeAdapter.kt
+++ app/src/main/java/com/example/forestlearning/StudytimeAdapter.kt
@@ -1,22 +1,29 @@
 package com.example.forestlearning
 
 import android.media.MediaCommunicationManager
+import android.os.Build
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.DiffUtil
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.forestlearning.databinding.FragmentSubjecttimerBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
-
-
-class StudytimeAdapter(
-    private val timerUpdateListener: TimerUpdateListener)
+class StudytimeAdapter()
     : ListAdapter<Subjects, StudytimeAdapter.Holder>(DiffCallback()) {
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private var viewModel: StudyTimeViewModel2 = StudyTimeViewModel2()
+
+    private var countDownTimer: CountDownTimer? = null
+    private var isTimerRunning = false
+    private var databaseReference: DatabaseReference = FirebaseDatabase.getInstance().reference
 
     interface TimerUpdateListener {
         fun onTimerUpdate(position: Int, time: Time)
@@ -26,15 +33,49 @@ class StudytimeAdapter(
         return Holder(binding)
     }
 
-
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: Holder, position: Int) {
         val data = getItem(position)
         holder.bind(data)
         holder.binding.playButton.setOnClickListener {
-            timerUpdateListener.onTimerUpdate(position, viewModel.currenttime.value!!)
+            if(isTimerRunning) {
+                StopTimer()
+            }else {
+                StartTimer(data)
+            }
         }
+        val position = viewModel.getSubjectsList().value?.indexOf(data)
+        println(position)
+        if (position != null && position != -1) {
+            viewModel.updateTime(position, data.time)
+        }
+        databaseReference.child(position.toString()).setValue(data)
     }
 
+    private fun StartTimer(subject: Subjects) {
+        countDownTimer = object: CountDownTimer(Long.MAX_VALUE, 1000){
+            override fun onTick(millisUtilFinished: Long) {
+                subject.time.sec++
+                if (subject.time.sec >= 60) {
+                    subject.time.sec = 0
+                    subject.time.minute++
+                }
+                if (subject.time.minute >= 60) {
+                    subject.time.minute = 0
+                    subject.time.hour++
+                }
+                notifyDataSetChanged()
+
+            }
+            override fun onFinish() {}
+            }.start()
+            isTimerRunning = true
+        }
+    private fun StopTimer() {
+        countDownTimer?.cancel()
+        countDownTimer = null
+        isTimerRunning = false
+    }
 
     class Holder(val binding: FragmentSubjecttimerBinding)
         : RecyclerView.ViewHolder(binding.root) {
@@ -65,4 +106,6 @@ class StudytimeAdapter(
         }
     }
 }
+
+
 

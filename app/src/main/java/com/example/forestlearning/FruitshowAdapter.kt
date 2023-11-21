d296f44b80e9ba7d71e2ca373449e568119be448
Mun Chae Yeong <99884555+mcy0325@users.noreply.github.com>
2023-11-21 11:02:38 +0900

    11. 21 mcy 1

--- app/src/main/java/com/example/forestlearning/FruitshowAdapter.kt
+++ app/src/main/java/com/example/forestlearning/FruitshowAdapter.kt
@@ -4,25 +4,55 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.forestlearning.databinding.FruitshowListBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
-class FruitshowAdapter(array:Array<FruitShowData>) : RecyclerView.Adapter<FruitshowAdapter.Holder>() {
-    inner class Holder(val binding: FruitshowListBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun dataBinding(data: FruitShowData) {
-            binding.userName.text = data.nickname
-            binding.userFruit.text = data.fruitnum.toString()
+class FruitshowAdapter : RecyclerView.Adapter<FruitshowAdapter.Holder>() {
+    //리사이클러뷰로 출력할 데이터 받는 곳
+    val fruitData : ArrayList<FruitShowData> = arrayListOf()
+
+    //firestore database에서 데이터 불러오기 위해 선언
+    val db : FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    //목록 띄우기
+
+    init {
+        db.collection("Users").addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            fruitData.clear()
+            //fruitData에 firestore 정보 넣기
+            if (querySnapshot != null) {
+                for(snapshot in querySnapshot.documents) {
+                    val item = snapshot.toObject(FruitShowData::class.java)
+                    if (item != null) {
+                        fruitData.add(item)
+                    }
+                }
+            }
+
+            fruitData.sortByDescending { it.fruitnum }
         }
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FruitshowAdapter.Holder {
+
+    //검색 기능 구현 함수
+    fun search(searchName : String) {
+
+    }
+
+    class Holder(val binding: FruitshowListBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun dataBind(data: FruitShowData, position: Int) {
+            binding.userName.text = data.nickname
+            binding.userFruit.text = data.fruitnum.toString()
+            binding.userRank.text = (position+1).toString()
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = FruitshowListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return Holder(binding)
     }
 
-    override fun onBindViewHolder(holder: FruitshowAdapter.Holder, position: Int) {
-        //holder.dataBinding(array[position])
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.dataBind(fruitData[position], position)
     }
 
-    override fun getItemCount(): Int {
-        //return array.size
-        return 0}
+    override fun getItemCount() = fruitData.size
 
 }

842ec717fd8064bddd8383f045075e40007c14aa
Mun Chae Yeong <99884555+mcy0325@users.noreply.github.com>
2023-12-05 02:35:17 +0900

    12. 05. mcy modify

--- app/src/main/java/com/example/forestlearning/HostActivity.kt
+++ app/src/main/java/com/example/forestlearning/HostActivity.kt
@@ -3,7 +3,7 @@ package com.example.forestlearning
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class hostActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_host)
--- app/src/main/java/com/example/forestlearning/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/forestlearning/viewmodel/UserViewModel.kt
@@ -57,6 +57,4 @@ class UserViewModel : ViewModel() {
             }
         })
     }
-
-
 }

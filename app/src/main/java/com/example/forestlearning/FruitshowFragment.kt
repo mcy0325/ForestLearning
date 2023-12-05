b13254b79f064d4e0152eb66ec2499a60bfa1399
Mun Chae Yeong <99884555+mcy0325@users.noreply.github.com>
2023-12-06 05:20:00 +0900

    12. 06. mcy modify2

--- app/src/main/java/com/example/forestlearning/FruitshowFragment.kt
+++ app/src/main/java/com/example/forestlearning/FruitshowFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.forestlearning.databinding.FragmentFruitshowBinding
+import com.example.forestlearning.viewmodel.FruitshowViewModel
 
 class FruitshowFragment : Fragment() {
     var binding: FragmentFruitshowBinding? = null
+    private val viewModel: FruitshowViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,25 +24,29 @@ class FruitshowFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    
-        //RecyclerView 설정
+
+        val adapter = FruitshowAdapter()
         binding?.fruitShowRecycler?.apply {
             layoutManager = LinearLayoutManager(context)
-            adapter = FruitshowAdapter()
+            this.adapter = adapter
         }
-        
-        //검색 기능 사용 (사용자 이름 검색)
+
+        viewModel.fruitData.observe(viewLifecycleOwner) { data ->
+            adapter.fruitData = data
+        }
+
         binding?.searchBtn?.setOnClickListener {
             val searchName = binding?.searchName?.text.toString()
             if (searchName.isNotEmpty()) {
                 // 검색어가 비어있지 않을 경우 검색 수행
-                (binding?.fruitShowRecycler?.adapter as FruitshowAdapter).search(searchName)
+                adapter.search(searchName)
             } else {
                 // 검색어가 비어있을 경우 전체 목록 출력
-                (binding?.fruitShowRecycler?.adapter as FruitshowAdapter).search("")
+                adapter.search("")
             }
         }
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         // 뷰가 파괴될 때 바인딩 객체를 null로 설정
---
+++ app/src/main/java/com/example/forestlearning/viewmodel/FruitshowViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.forestlearning.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.forestlearning.FruitShowData
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class FruitshowViewModel : ViewModel() {
+    private val db : DatabaseReference = FirebaseDatabase.getInstance().getReference("Users")
+
+    // LiveData for fruitData
+    private val _fruitData = MutableLiveData<List<FruitShowData>>()
+    val fruitData: LiveData<List<FruitShowData>>
+        get() = _fruitData
+
+    init {
+        fetchFruitData()
+    }
+
+    private fun fetchFruitData() {
+        db.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val temp = mutableListOf<FruitShowData>()
+                for(userSnapshot in dataSnapshot.children) {
+                    val userName = userSnapshot.child("name").getValue(String::class.java) ?: ""
+                    var totalFruits = 0
+                    val dayFruitSnapshot = userSnapshot.child("Dayfruit")
+                    for(dateSnapshot in dayFruitSnapshot.children) {
+                        for(fruitSnapshot in dateSnapshot.children) {
+                            val fruitNum = fruitSnapshot.getValue(Int::class.java) ?: 0
+                            totalFruits += fruitNum
+                        }
+                    }
+                    val item = FruitShowData(userName, totalFruits)
+                    temp.add(item)
+                }
+                temp.sortByDescending { it.fruitnum }
+                _fruitData.value = temp
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // handle error
+            }
+        })
+    }
+}
